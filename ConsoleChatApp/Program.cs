f1f7146964b87d8336975b20b9a67ebfcc4828cb
Quy <tranphuquy19@gmail.com>
2020-08-19 21:14:19 +0700

    first commit

---
+++ ConsoleChatApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
